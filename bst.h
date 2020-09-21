c881018fdfe8967bb1633b8ba32f25bfc05633ea
Pablo Lamtenzan <pablolamtenzan@gmail.com>
2020-09-21 18:23:34 +0200

    New miniSH, engine building end

--- bst.h
+++ bst.h
@@ -4,7 +4,7 @@
 #define NONE					0
 #define PIPE					1
 #define REDIRECTION_GREATHER	2
-#define REFIRECTION_LESSER		4
+#define REDIRECTION_LESSER		4
 #define REDIRECTION_DGREATHER	8
 #define SEMICOLON				16
 
@@ -17,5 +17,6 @@ typedef struct					s_bst
 }								t_bst;
 
 t_bst							*new_node(const unsigned char operator, char *cmd[2], t_bst *back);
+t_bst							*build_bst(char *operators, char **cmds);
 
 #endif
