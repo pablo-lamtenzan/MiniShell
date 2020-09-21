e08a06b1cdfa4f0e12843121c5d378b15ac95f15
Pablo Lamtenzan <pablolamtenzan@gmail.com>
2020-09-21 18:07:15 +0200

    New miniSH, engine building end

---
+++ bst.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bst.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plamtenz <plamtenz@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/21 17:23:02 by plamtenz          #+#    #+#             */
+/*   Updated: 2020/09/21 18:06:13 by plamtenz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <bst.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+t_bst		*new_node(const unsigned char operator, char *cmd[2], t_bst *back)
+{
+	t_bst	*new;
+
+	if (!(new = (t_bst *)malloc(sizeof(t_bst))))
+		return (NULL);
+	if (back)
+		back->next = new;
+	new->back = back;
+	new->cmd[0] = cmd[0];
+	new->cmd[1] = cmd[1];
+	new->operator = operator;
+	return (new);
+}
+
+// number of bts == number of semicolons + 1
+t_bst		*build_bst(char *operators, char **cmds)
+{
+	t_bst	*tail;
+	t_bst	*head;
+	char	*cmds_format_conv[2];
+	int		it[2];
+
+	tail = NULL;
+	it[0] = 0;
+	it[1] = 0;
+	while (*(operators + it[0]))
+	{
+		if (tail)
+		{
+			cmds_format_conv[0] = NULL;
+			cmds_format_conv[1] = *(cmds + it[1]++);
+		}
+		else
+		{
+			cmds_format_conv[0] = *(cmds + it[1]++);
+			cmds_format_conv[1] = *(cmds + it[1]++);
+		}
+		if (tail == NULL)
+			head = tail;
+		if (!(tail = new_node(*(operators + it[0]++), cmds_format_conv, tail))
+				|| *(cmds + it[1]) == NULL)
+			return (NULL);
+	}
+	return (head);
+}
+/* The previous fct should return the head of the execution,
+	the execution is composed by a binary search tree. The first node
+	should be composed by an operator and 2 cmds (1 before and 1 after).
+	If there are more than 1 node the next nodes should have only 1 cmd beacuse
+	the other is the previous execution.
+
+	If this is right, the engine is built in this function.
+*/
+
---
+++ bst.h
@@ -0,0 +1,21 @@
+#ifndef BST_H
+# define BST_H
+
+#define NONE					0
+#define PIPE					1
+#define REDIRECTION_GREATHER	2
+#define REFIRECTION_LESSER		4
+#define REDIRECTION_DGREATHER	8
+#define SEMICOLON				16
+
+typedef struct					s_bst
+{
+	unsigned char				operator;
+	char						*cmd[2];
+	struct s_bst				*next;
+	struct s_bst				*back;
+}								t_bst;
+
+t_bst							*new_node(const unsigned char operator, char *cmd[2], t_bst *back);
+
+#endif
