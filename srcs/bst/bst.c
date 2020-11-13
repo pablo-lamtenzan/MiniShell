973528724eb5fef70f4a475b13217990dc696e12
Clément Hamada <chamada@student.le-101.fr>
2020-11-13 03:21:53 +0100

    bst: Add comments on static analysis errors

--- srcs/bst/bst.c
+++ srcs/bst/bst.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   bst.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pablo <pablo@student.42.fr>                +#+  +:+       +#+        */
+/*   By: chamada <chamada@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 16:23:23 by pablo             #+#    #+#             */
-/*   Updated: 2020/11/04 23:37:00 by pablo            ###   ########.fr       */
+/*   Updated: 2020/11/12 20:52:18 by chamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 static t_bst*	build_job(t_tok* tokens, t_tok* delim)
 {
-    t_tok* 		tk1;
-	t_tok*		tk2;
-	t_bst*		node;
+	t_tok*	tk1;
+	t_tok*	tk2;
+	t_bst*	node;
 
 	tk1 = find_last_operator(tokens, delim);
-    node = new_node(NULL, NULL, tk1->type);
+	node = new_node(NULL, NULL, tk1->type);
 	tk2 = find_next_operator(tokens, REDIR_GR | REDIR_LE | REDIR_DG | PIPE);
 	if (tk1 != tk2 && tk2 != delim)
 	{
 		node->a = build_job(tokens, tk1);
-        node->b = tk1->data;
+		node->b = tk1->data;
 	}
 	else
 	{
 		if (tokens->type & CMD)
-            node->a = tokens->data;
+			node->a = tokens->data;
 		if (tk1->type & (REDIR_GR | REDIR_LE | REDIR_DG))
-            node->b = tk1->data;
+			node->b = tk1->data;
 		node->type |= CMD;
 	}
 	return (node);
@@ -39,24 +39,24 @@ static t_bst*	build_job(t_tok* tokens, t_tok* delim)
 
 static t_bst*	build_bst(t_tok* tokens)
 {
-    t_tok*		tk1;
-	t_tok*		tk2;
-	t_tok*		tk3;
-    t_bst*		node;
+	t_tok*	tk1;
+	t_tok*	tk2;
+	t_tok*	tk3;
+	t_bst*	node;
 
 	if ((tk1 = find_next_operator(tokens, PIPE))->type & PIPE)
-    	node = new_node(NULL, NULL, tk1->type);
+		node = new_node(NULL, NULL, tk1->type);
 	else if ((tk3 = find_next_operator(tokens, REDIR_GR | REDIR_LE \
-             | REDIR_DG))->type & (REDIR_GR | REDIR_LE | REDIR_DG))
-        node = new_node(NULL, NULL, tk3->type);
+			 | REDIR_DG))->type & (REDIR_GR | REDIR_LE | REDIR_DG))
+		node = new_node(NULL, NULL, tk3->type);
 	else
 		node = new_node(NULL, NULL, CMD);
-    node->a = build_job(tokens, tk1);
-    if ((tk2 = find_next_operator(tk1->next, PIPE))->type & PIPE)
-        node->b = build_bst(tk1->next);
+	node->a = build_job(tokens, tk1);
+	if ((tk2 = find_next_operator(tk1->next, PIPE))->type & PIPE)
+		node->b = build_bst(tk1->next); // TODO: dead assignment tk2 (unused)
 	else if ((tk3 = find_next_operator(tk1, REDIR_GR | REDIR_LE \
-             | REDIR_DG))->type & (REDIR_GR | REDIR_LE | REDIR_DG))
-			node->b = build_job(tk1->next, NULL);
+			 | REDIR_DG))->type & (REDIR_GR | REDIR_LE | REDIR_DG))
+			node->b = build_job(tk1->next, NULL); // TODO: dead assignment tk3 (unused)
 	else
 		node->b = build_job(tk1->next, NULL);
 	return (node);
