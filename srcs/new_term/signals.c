bd60810ed04b1e504a9b2655bc66c463f9981159
Clément Hamada <clementhamada@protonmail.com>
2020-11-21 17:08:32 +0100

    term: Implement ctrl+c

--- srcs/new_term/signals.c
+++ srcs/new_term/signals.c
@@ -1,16 +1,27 @@
 #include "term.h"
 
-t_term_err	term_interrupt(t_term *t)
+t_term_err	term_interrupt(t_term *term)
 {
-	(void)t;
-	ft_dprintf(2, "[PROMPT][SIGNAL] INTERRUPT\n");
+	if (term->msg && (write(term->fds[2], TERM_ENDL, sizeof(TERM_ENDL) -1) == -1
+	|| ((term->origin = ft_strlen(term->msg))
+	&& write(term->fds[2], term->msg, term->origin) == -1)))
+		return (TERM_EWRITE);
+	if (term->has_caps)
+		select_clear(term);
+	if (term->line != term->hist.next)
+	{
+		line_clear(&term->line);
+		term->hist.curr = term->hist.next;
+		term->line = term->hist.next;
+	}
+	*term->line->data = '\0';
+	term->line->len = 0;
+	term->pos = 0;
 	return (TERM_EOK);
 }
 
 t_term_err	term_eof(t_term *t)
 {
-	(void)t;
-	ft_dprintf(2, "[PROMPT][SIGNAL] EOF\n");
 	if (t->line->len == 0)
 		return (TERM_EEOF);
 	return (TERM_EOK);
