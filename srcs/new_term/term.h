2cc3125e37c109444dd20b8ec8672e83baae970a
Clément Hamada <clementhamada@protonmail.com>
2020-11-23 11:12:06 +0100

    term: Format header

--- srcs/new_term/term.h
+++ srcs/new_term/term.h
@@ -35,7 +35,7 @@
 
 # define TERM_ENDL		"\n"
 
-typedef enum	e_term_err
+typedef enum		e_term_err
 {
 	TERM_EALLOC = -3,
 	TERM_EWRITE = -2,
@@ -43,28 +43,28 @@ typedef enum	e_term_err
 	TERM_EEOF = 0,
 	TERM_EOK = 1,
 	TERM_ENL = 2
-}				t_term_err;
+}					t_term_err;
 
-typedef struct	s_select
+typedef struct		s_select
 {
 	size_t	start;
 	size_t	end;
-}				t_select;
+}					t_select;
 
-typedef struct	s_clip
+typedef struct		s_clip
 {
 	t_line		line;
 	t_select	select;
-}				t_clip;
+}					t_clip;
 
-typedef struct	s_hist
+typedef struct		s_hist
 {
 	t_line	*last;
 	t_line	*curr;
 	t_line	*next;
-}				t_hist;
+}					t_hist;
 
-typedef struct	s_term
+typedef struct		s_term
 {
 	int			fds[3];
 	t_caps		caps;
@@ -76,91 +76,91 @@ typedef struct	s_term
 	size_t		origin;
 	size_t		pos;
 	t_clip		clip;
-}				t_term;
+}					t_term;
 
-typedef t_term_err (*t_term_action)(t_term *term);
+typedef t_term_err	(*t_term_action)(t_term *term);
 
-typedef struct	s_keybind
+typedef struct		s_keybind
 {
 	char			key;
 	t_term_action	action;
-}				t_keybind;
+}					t_keybind;
 
 /*
-**				term.c
+**					term.c
 */
-bool			term_init(t_term *term, t_env **env);
-void			term_destroy(t_term *term);
-t_term_err		term_prompt(t_term *term);
+bool				term_init(t_term *term, t_env **env);
+void				term_destroy(t_term *term);
+t_term_err			term_prompt(t_term *term);
 
 /*
-**				caps.c
+**					caps.c
 */
-bool			term_init_caps(t_term *term, t_env **env);
+bool				term_init_caps(t_term *term, t_env **env);
 
 /*
-**				read.c
+**					read.c
 */
-t_term_err		term_read_caps(t_term *term);
-t_term_err		term_read(t_term *term);
+t_term_err			term_read_caps(t_term *term);
+t_term_err			term_read(t_term *term);
 
 /*
-**				read_esc.c
+**					read_esc.c
 */
-t_term_err		term_read_esc(t_term *term);
+t_term_err			term_read_esc(t_term *term);
 
 /*
-**				controls.c
+**					controls.c
 */
-t_term_err		term_clear_screen(t_term *term);
-t_term_err		term_backspace(t_term *term);
-t_term_err		term_clear_line(t_term *term);
-t_term_err		term_new_line(t_term *term);
+t_term_err			term_clear_screen(t_term *term);
+t_term_err			term_backspace(t_term *term);
+t_term_err			term_clear_line(t_term *term);
+t_term_err			term_new_line(t_term *term);
 
 /*
-**				cursor.c
+**					cursor.c
 */
-t_term_err		cursor_l(t_term *term);
-t_term_err		cursor_r(t_term *term);
-t_term_err		cursor_start_line(t_term *term);
-t_term_err		cursor_end_line(t_term *term);
+t_term_err			cursor_l(t_term *term);
+t_term_err			cursor_r(t_term *term);
+t_term_err			cursor_start_line(t_term *term);
+t_term_err			cursor_end_line(t_term *term);
 
 /*
 **				select.c
 */
-t_term_err		select_left(t_term *t);
-t_term_err		select_right(t_term *t);
-t_term_err		select_clear(t_term *t);
+t_term_err			select_left(t_term *t);
+t_term_err			select_right(t_term *t);
+t_term_err			select_clear(t_term *t);
 
 /*
-**				clip.c
+**					clip.c
 */
-t_term_err		clip_copy(t_term *t);
-t_term_err		clip_cut(t_term *t);
-t_term_err		clip_paste(t_term *t);
-t_term_err		clip_clear(t_term *t);
+t_term_err			clip_copy(t_term *t);
+t_term_err			clip_cut(t_term *t);
+t_term_err			clip_paste(t_term *t);
+t_term_err			clip_clear(t_term *t);
 
 /*
-**				hist_cursor.c
+**					hist_cursor.c
 */
-t_term_err		term_prev_line(t_term *t);
-t_term_err		term_next_line(t_term *t);
+t_term_err			term_prev_line(t_term *t);
+t_term_err			term_next_line(t_term *t);
 
 
 /*
-**				hist.c
+**					hist.c
 */
-void			hist_add(t_hist *hist, t_line *line);
-void			hist_pop(t_hist *hist);
-void			hist_clear(t_hist *hist);
-void			hist_commit(t_hist *hist, t_line *line);
+void				hist_add(t_hist *hist, t_line *line);
+void				hist_pop(t_hist *hist);
+void				hist_clear(t_hist *hist);
+void				hist_commit(t_hist *hist, t_line *line);
 
 /*
-**				signals.c
+**					signals.c
 */
-t_term_err		term_interrupt(t_term *t);
-t_term_err		term_eof(t_term *t);
-t_term_err		term_stop(t_term *t);
-t_term_err		term_suspend(t_term *t);
+t_term_err			term_interrupt(t_term *t);
+t_term_err			term_eof(t_term *t);
+t_term_err			term_stop(t_term *t);
+t_term_err			term_suspend(t_term *t);
 
 #endif
