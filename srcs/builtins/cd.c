da495259ea9cd445ae3d78c4b3147317850829f5
Pablo Lamtenzan <pablolamtenzan@gmail.com>
2020-12-02 10:24:02 +0100

    solve piped cd commands

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,13 +6,25 @@
 /*   By: pablo <pablo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/22 21:57:11 by plamtenz          #+#    #+#             */
-/*   Updated: 2020/12/01 14:47:10 by pablo            ###   ########.fr       */
+/*   Updated: 2020/12/02 10:22:44 by pablo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <execution.h>
 #include <string.h>
 // TODO: Check allocation errors on map_set
+
+
+int	ft_chdir(const char *path)
+{
+	struct stat stats;
+
+	stat(path, &stats);
+	if (S_ISDIR(stats.st_mode) && g_session.flags & PIPED_CMD)
+		return (0);
+	else
+		return (chdir(path));
+}
 
 int	b_cd(t_exec *args)
 {
@@ -27,7 +39,7 @@ int	b_cd(t_exec *args)
 			ft_dprintf(STDERR_FILENO, "%s: %s: HOME not set\n", g_session.name, args->av[0]);
 			return (STD_ERROR);
 		}
-		if (chdir(home_dir))
+		if (ft_chdir(home_dir))
 		{
 			ft_dprintf(STDERR_FILENO, "%s: %s: %s\n", g_session.name, args->av[0], strerror(errno));
 			return (STD_ERROR);
@@ -39,7 +51,7 @@ int	b_cd(t_exec *args)
 	{
 		ft_bzero(path, PATH_MAX);
 		ft_memcpy(path, args->av[1], ft_strlen(args->av[1]));
-		if (path[0] == '/' && chdir(path) == 0)
+		if (path[0] == '/' && ft_chdir(path) == 0)
 		{
 			env_set(&g_session.env, "PWD", path, true);
 			return (SUCCESS);
@@ -50,7 +62,7 @@ int	b_cd(t_exec *args)
 				return (1);
 			ft_strlcat(cwd, "/", ft_strlen(cwd) + 2);
 			ft_strlcat(cwd, path, ft_strlen(cwd) + 2 + ft_strlen(path));
-			if (chdir(cwd) == 0)
+			if (ft_chdir(cwd) == 0)
 			{
 				getcwd(cwd, sizeof(cwd));
 				env_set(&g_session.env, "PWD", cwd, true);
