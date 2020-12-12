d0f40db4588765c3931959a73a6d0517e5f87e41
Clément Hamada <chamada@student.le-101.fr>
2020-12-12 19:22:15 +0100

    main: Handle lexer allocation error

--- srcs/main/main.c
+++ srcs/main/main.c
@@ -154,22 +154,20 @@ static t_term_err	routine(void)
 	ft_bzero(&lex_data, sizeof(lex_data));
 	status = TERM_EOK;
 	lex_status = LEX_EOK;
-	while ((!g_term.is_interactive || (g_term.msg = msg_get(lex_status)))
+	while (lex_status != LEX_EALLOC
+	&& (!g_term.is_interactive || (g_term.msg = msg_get(lex_status)))
 	&& (status = term_prompt(&lex_data.input)) == TERM_ENL)
 	{
 		if ((lex_status = lex_tokens(&tokens, &lex_data)) == LEX_EOK)
-		{
 			exec(&tokens);
-			token_clr(&tokens);
-		}
-		else if (lex_status != LEX_EWAIT)
-		{
+		else if (lex_status == LEX_ESYNTAX)
 			syntax_error(&lex_data);
+		if (lex_status != LEX_EWAIT && tokens)
 			token_clr(&tokens);
-		}
 		line_clear(&g_term.msg);
 	}
-	return ((g_term.is_interactive && !g_term.msg) ? TERM_EALLOC : status);
+	return ((lex_status == LEX_EALLOC || (g_term.is_interactive && !g_term.msg))
+		? TERM_EALLOC : status);
 }
 
 int						main(int ac, const char **av, const char **ep)
